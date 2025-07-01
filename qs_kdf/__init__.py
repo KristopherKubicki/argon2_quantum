99450940b6d47f05b3d316319d62a65f06ab529c
Kristopher Kubicki <478212+KristopherKubicki@users.noreply.github.com>
2025-06-30 20:08:22 -0500

    🍃style: reorder imports for isort compliance

--- qs_kdf/__init__.py
+++ qs_kdf/__init__.py
@@ -1,7 +1,7 @@
 """Quantum stretch KDF package."""
 
-from .core import lambda_handler, hash_password, LocalBackend
 from .cli import main as cli
+from .core import LocalBackend, hash_password, lambda_handler
 from .test_backend import TestBackend
 
 __all__ = ["lambda_handler", "cli", "TestBackend", "hash_password", "LocalBackend"]
